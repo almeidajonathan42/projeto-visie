c0d2eb1f61968e7d5452dd4ba33ad447c4285f96
Jonathan Rocha <jonathan.rocha@msn.com>
2023-07-13 21:11:44 -0300

    Implement Ler Depois page

--- src/app/ler-depois/page.tsx
+++ src/app/ler-depois/page.tsx
@@ -1,7 +1,47 @@
-export default function Page() {
+import styles from "./page.module.css";
+import { prisma } from "@/db";
+import { getData } from "@/functions";
+import ArticleItem from "../../components/ArticleItem";
+import BackButton from "@/components/BackButton";
+
+export default async function Page() {
+  const articleDBItems = await prisma.article.findMany({
+    where: { isReadLater: true },
+  });
+
+  const articles = await Promise.all(
+    articleDBItems.map(async (item) => {
+      const data = await getData(
+        "https://endeavor.org.br/wp-json/wp/v2/posts?include=" + item.id
+      );
+      return data[0];
+    })
+  );
+
   return (
-    <main>
-      <h1>Ler Depois</h1>
+    <main className={styles.container}>
+      {articles.length === 0 && (
+        <div className={styles.emptyContent}>
+          <p> Você ainda não marcou nenhum item para ler depois :/ </p>
+        </div>
+      )}
+
+      {articles.map((article: any) => {
+        return (
+          <div key={article.id}>
+            <ArticleItem
+              id={article.id}
+              image={article.yoast_head_json.og_image[0].url}
+              title={article.title.rendered}
+              description={article.excerpt.rendered}
+              date={article.date}
+              link={article.link}
+            />
+          </div>
+        );
+      })}
+
+      {articles.length > 5 && <BackButton />}
     </main>
   );
 }
