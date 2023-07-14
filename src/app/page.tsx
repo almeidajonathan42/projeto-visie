688039439129e901515e742459f7352f170627bc
Jonathan Rocha <jonathan.rocha@msn.com>
2023-07-14 15:45:20 -0300

    Add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 import ArticleItem from "../components/ArticleItem";
 import Pagination from "../components/Pagination";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 const categories = [
   { id: "1309", name: "Desenvolvimento Pessoal" },
@@ -9,19 +9,33 @@ const categories = [
   { id: "1317", name: "Gestão de Pessoas" },
 ];
 
-export default async function PaginaInicial(props: any) {
+let currentPage = 1;
 
+export default async function PaginaInicial(props: any) {
   if (props.searchParams.categories == null) {
-    redirect('/?categories=1309,1288,1317');
+    redirect("/?categories=1309,1288,1317");
   }
 
   const categories = props.searchParams.categories;
-  const url = "https://endeavor.org.br/wp-json/wp/v2/posts?categories=" + categories;
+  const url =
+    "https://endeavor.org.br/wp-json/wp/v2/posts?categories=" + categories + "&page=" + currentPage;
   const res = await fetch(url);
   const articles = await res.json();
   const pageTotal = res.headers.get("X-WP-TotalPages") || "1";
 
-  let currentPage = 1;
+  
+
+  async function handleForwardPagination(arg: any) {
+    "use server"
+
+    currentPage++;
+  }
+
+  async function handleBackwardsPagination(arg: any) {
+    "use server"
+
+    currentPage--;
+  }
 
   return (
     <main className={styles.container}>
@@ -40,7 +54,12 @@ export default async function PaginaInicial(props: any) {
         );
       })}
 
-      <Pagination currentPage={currentPage} pageTotal={parseInt(pageTotal)} />
+      <Pagination
+        currentPage={currentPage}
+        pageTotal={parseInt(pageTotal)}
+        onForward={handleForwardPagination}
+        onBackwards={handleBackwardsPagination}
+      />
     </main>
   );
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,24 +6,37 @@ import { useRouter } from "next/navigation";
 interface Props {
     currentPage: number;
     pageTotal: number;
+    onForward: () => void;
+    onBackwards: () => void;
 }
 
-export default function BackButton(props: Props) {
+export default function BackButton(props: any) {
   const route = useRouter();
 
   return (
     <div className={styles.container}>
-      <p> Página </p>
-      <input className={styles.page} type="number" value={props.currentPage} />
-      <p> de {props.pageTotal} </p>
       <button
         className={styles.button}
         onClick={(e: any) => {
-          console.log("bora");
-          route.refresh();
+          if (props.currentPage > 1) {
+            props.onBackwards();
+            route.refresh();
+          }
         }}
       >
-        Ir
+        Voltar
+      </button>
+      <p> Página {props.currentPage} de {props.pageTotal} </p>
+      <button
+        className={styles.button}
+        onClick={(e: any) => {
+          if(props.currentPage < props.pageTotal) {
+            props.onForward();
+            route.refresh();
+          }
+        }}
+      >
+        Avançar
       </button>
     </div>
   );
