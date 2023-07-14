e4abda39231fa2385e2dbb38ad59de1c46f59ad5
Jonathan Rocha <jonathan.rocha@msn.com>
2023-07-14 15:20:30 -0300

    Change url on BackButton

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -4,5 +4,5 @@ import Link from 'next/link';
 import styles from "./BackButton.module.css";
 
 export default function BackButton() {
-  return <Link className={styles.back} href="/"> Voltar para página inicial </Link>;
+  return <Link className={styles.back} href="/?categories=1309,1288,1317"> Voltar para página inicial </Link>;
 }
