5b14eeccf92316d0b49e4c90ae56068304656be3
Jonathan Rocha <jonathan.rocha@msn.com>
2023-07-14 11:01:50 -0300

    Remove styled-componets from project

--- src/components/NoteWrite.tsx
+++ src/components/NoteWrite.tsx
@@ -4,41 +4,6 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./NoteWrite.module.css";
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 40px;
-  align-items: flex-end;
-`;
-
-const Textarea = styled.textarea`
-  height: 200px;
-  width: 700px;
-  margin-bottom: 15px;
-  padding: 12px 20px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  background-color: #f8f8f8;
-  font-size: 16px;
-  resize: none;
-  font-family: inherit;
-  margin-top: 20px;
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background: #09e1cd;
-  border: none;
-  border-radius: 3px;
-  padding: 10px 15px;
-  font-size: 16px;
-  font-weight: 500;
-`;
-
 interface Props {
   handleSaveNote: (note: string) => void;
   existingNote: string;
