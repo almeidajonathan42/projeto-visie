2c497bd66a5febec465fd294cbd54f0e0047b0c0
Jonathan Rocha <jonathan.rocha@msn.com>
2023-07-13 21:05:43 -0300

    Move key to prevet React bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,22 @@ import ArticleItem from "../components/ArticleItem";
 import { getData } from "../functions";
 
 export default async function PaginaInicial() {
-
   const articles = await getData("https://endeavor.org.br/wp-json/wp/v2/posts");
 
   return (
     <main className={styles.container}>
       {articles.map((article: any) => {
         return (
-          <ArticleItem
-            id={article.id}
-            image={article.yoast_head_json.og_image[0].url}
-            title={article.title.rendered}
-            description={article.excerpt.rendered}
-            date={article.date}
-            link={article.link}
-          />
+          <div key={article.id}>
+            <ArticleItem
+              id={article.id}
+              image={article.yoast_head_json.og_image[0].url}
+              title={article.title.rendered}
+              description={article.excerpt.rendered}
+              date={article.date}
+              link={article.link}
+            />
+          </div>
         );
       })}
     </main>
